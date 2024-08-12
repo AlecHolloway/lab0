a4bc5b8f17f8e0a610834fbb647a9e4976d3a13c
AlecHolloway <41766467+AlecHolloway@users.noreply.github.com>
2024-08-12 16:45:38 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"main/handlers"
-	"net/http"
+        "log"
+        "main/handlers"
+        "net/http"
 )
 
 func main() {
-	// TODO: some code goes here
-	// Fill out the HomeHandler function in handlers/handlers.go which handles the user's GET request.
-	// Start an http server using http.ListenAndServe that handles requests using HomeHandler.
+        // TODO: some code goes here
+        // Fill out the HomeHandler function in handlers/handlers.go which handles the user's GET request.
+        // Start an http server using http.ListenAndServe that handles requests using HomeHandler.
+        http.HandleFunc("/", handlers.HomeHandler)
+
+        log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
