b4c07fe98709c8228a54e5799f4b385daaf21417
AlecHolloway <41766467+AlecHolloway@users.noreply.github.com>
2024-08-12 16:46:23 -0400

    Update handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,62 +1,78 @@
 package handlers
 
 import (
-	"encoding/base64"
-	"fmt"
-	"html/template"
-	rdb "main/ridership_db"
-	"main/utils"
-	"net/http"
-	"os"
-	"path/filepath"
-	"runtime"
+        "encoding/base64"
+        "fmt"
+        "html/template"
+        rdb "main/ridership_db"
+        "main/utils"
+        "net/http"
+        "os"
+        "path/filepath"
+        "runtime"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	// Get the selected chart from the query parameter
-	selectedChart := r.URL.Query().Get("line")
-	if selectedChart == "" {
-		selectedChart = "red"
-	}
+        // Get the selected chart from the query parameter
+        selectedChart := r.URL.Query().Get("line")
+        if selectedChart == "" {
+                selectedChart = "red"
+        }
+        // instantiate ridershipDB
+        //var db rdb.RidershipDB = &rdb.SqliteRidershipDB{} // Sqlite implementation
+        var db rdb.RidershipDB = &rdb.CsvRidershipDB{} // CSV implementation
 
-	// instantiate ridershipDB
-	var db rdb.RidershipDB = &rdb.SqliteRidershipDB{} // Sqlite implementation
-	// var db rdb.RidershipDB = &rdb.CsvRidershipDB{} // CSV implementation
+        // TODO: some code goes here
+        // Get the chart data from RidershipDB
+        err := db.Open("mbta.csv")
+        if err != nil {
+                fmt.Printf("Could not open file\n")
+        }
+        contents, err := db.GetRidership(selectedChart)
+        if err != nil {
+                fmt.Printf("Could not get contents\n")
+        }
+        // TODO: some code goes here
+        // Plot the bar chart using utils.GenerateBarChart. The function will return the bar chart
+        // as PNG byte slice. Convert the bytes to a base64 string, which is used to embed images in HTML.
+        barChartBytes, err := utils.GenerateBarChart(contents)
+        if err != nil {
+                fmt.Printf("err generating bar chart bytes\n")
+        }
+        barChartStr := base64.StdEncoding.EncodeToString(barChartBytes)
 
-	// TODO: some code goes here
-	// Get the chart data from RidershipDB
+        // Get path to the HTML template for our web app
+        _, currentFilePath, _, _ := runtime.Caller(0)
+        templateFile := filepath.Join(filepath.Dir(currentFilePath), "template.html")
 
-	// TODO: some code goes here
-	// Plot the bar chart using utils.GenerateBarChart. The function will return the bar chart
-	// as PNG byte slice. Convert the bytes to a base64 string, which is used to embed images in HTML.
+        // Read and parse the HTML so we can use it as our web app template
+        html, err := os.ReadFile(templateFile)
+        if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
+        tmpl, err := template.New("line").Parse(string(html))
+        if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
 
-	// Get path to the HTML template for our web app
-	_, currentFilePath, _, _ := runtime.Caller(0)
-	templateFile := filepath.Join(filepath.Dir(currentFilePath), "template.html")
+        // TODO: some code goes here
+        // We now want to create a struct to hold the values we want to embed in the HTML
+        data := struct {
+                Image string
+                Chart string
+        }{
+                Image: barChartStr, // TODO: base64 string
+                Chart: selectedChart,
+        }
 
-	// Read and parse the HTML so we can use it as our web app template
-	html, err := os.ReadFile(templateFile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	tmpl, err := template.New("line").Parse(string(html))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+        // TODO: some code goes here
+        // Use tmpl.Execute to generate the final HTML output and send it as a response
+        // to the client's request.
 
-	// TODO: some code goes here
-	// We now want to create a struct to hold the values we want to embed in the HTML
-	data := struct {
-		Image string
-		Chart string
-	}{
-		Image: "", // TODO: base64 string
-		Chart: selectedChart,
-	}
-
-	// TODO: some code goes here
-	// Use tmpl.Execute to generate the final HTML output and send it as a response
-	// to the client's request.
+        err = tmpl.Execute(w, data)
+        if err != nil {
+                fmt.Printf("err executing template\n")
+        }
 }
